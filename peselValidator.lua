2fcdfcbb55bd06c6135b17ee8e0ef5db9c35d1c6
Krzysztof Rudzki <krzysztof.rudzki@live.umcs.edu.pl>
2020-02-23 21:10:33 +0100

    Change a small error in code.

--- peselValidator.lua
+++ peselValidator.lua
@@ -4,7 +4,7 @@ local weightNumberInPESEL = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3}
 local sum = 0
 local controlNumber = tonumber(string.sub(inputString, 11))
 
-if ((tonumber(inputString) == false) or (#inputString ~= 11)) then
+if ((tonumber(inputString) == nil) or (#inputString ~= 11)) then
     return false
 else
     for i = 1, #inputString-1 do
